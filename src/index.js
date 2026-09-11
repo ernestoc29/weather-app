a4321910db3569e34577f848b723a360fb30b633
ernesto <274116253+ernestoc29@users.noreply.github.com>
2026-09-11 18:06:08 -0400

    connect location input to weather api

--- src/index.js
+++ src/index.js
@@ -1,1 +1,12 @@
+import { apiCall } from "./modules/api.js";
+import { processWeatherData } from "./modules/weather-processor.js";
 import "./styles/styles.css";
+
+const userInput = document.querySelector("input");
+const searchBtn = document.querySelector(".search");
+
+searchBtn.addEventListener("click", async () => {
+  const data = await apiCall.getWeather(userInput.value);
+  const weather = processWeatherData(data);
+  console.log(weather);
+});
--- src/modules/api.js
+++ src/modules/api.js
@@ -6,7 +6,7 @@ export const apiCall = {
       );
 
       const data = await response.json();
-      console.log(data);
+      return data;
     } catch (error) {
       console.error(error);
     }
